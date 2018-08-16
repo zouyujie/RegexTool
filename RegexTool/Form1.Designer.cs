9b67417045aca39936abb4661f63cf9885e81767
zouyujie <zouyujie@126.com>
2018-08-16 11:10:35 +0800

    init project

---
+++ RegexTool/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace RegexTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lstResult = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lstResult);
+            this.groupBox1.Controls.Add(this.btnTest);
+            this.groupBox1.Controls.Add(this.txtContent);
+            this.groupBox1.Controls.Add(this.txtRegex);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(665, 400);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "正则表达式：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "待匹配文本：";
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Location = new System.Drawing.Point(91, 21);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(551, 21);
+            this.txtRegex.TabIndex = 1;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(91, 49);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(551, 81);
+            this.txtContent.TabIndex = 2;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(311, 145);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 30);
+            this.btnTest.TabIndex = 3;
+            this.btnTest.Text = "测试匹配";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lstResult
+            // 
+            this.lstResult.FormattingEnabled = true;
+            this.lstResult.ItemHeight = 12;
+            this.lstResult.Location = new System.Drawing.Point(91, 190);
+            this.lstResult.Name = "lstResult";
+            this.lstResult.Size = new System.Drawing.Size(551, 136);
+            this.lstResult.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "匹配结果：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 416);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "正则表达式校验工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstResult;
+    }
+}
+
---
+++ RegexTool/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegexTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            string str =txtContent.Text;
+            string msg, rex;
+            Match m;
+            msg = txtContent.Text.Trim();
+            rex = @txtRegex.Text.Trim();
+            m = Regex.Match(msg, rex);
+
+            WriteGroupInfo(m);
+        }
+        private  void WriteGroupInfo(Match m)
+        {
+            lstResult.Items.Clear();
+            if (m.Success)
+            {
+                var g = m.Groups;
+                if (g.Count > 0)
+                {
+                    lstResult.Items.Add("序号     匹配值");
+                    for (var i = 0; i < g.Count; i++)
+                    {
+                        lstResult.Items.Add(i + ":     " + g[i].Value);// Environment.NewLine;
+                    }
+                }
+            }
+            else
+            {
+                lstResult.Items.Add("匹配失败");
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtContent.Text = "建筑：长沙市口腔医院，在15日产生以下报警：2#空调主机未关，请及时处理。";
+            txtRegex.Text = "(.*建筑：)(.*)，在([0-9]{1,})日产生以下报警：(.*)，请及时处理。";
+        }
+    }
+}
